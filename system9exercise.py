d7cbb415d3668f97bd167311b361d551e28eccd6
WontknoW <blajacyneobs@gmail.com>
2022-01-08 23:40:18 +0200

    Added update_orderbook function
    Function added to clean up duplicate code

--- system9exercise.py
+++ system9exercise.py
@@ -5,18 +5,17 @@ def cancel_tasks() -> None:
     for task in asyncio.all_tasks():
         task.cancel()
 
-
-async def depth_snapshot(q: asyncio.Queue, order_book: dict) -> None:
+async def depth_snapshot(q: asyncio.Queue, orderbook: dict) -> None:
     while q.empty(): await asyncio.sleep(0.0000001)
     while True:
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get('https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=1000') as resp:
                     resp = await resp.json()
-                    order_book['lastUpdateId'] = resp['lastUpdateId']
-                    order_book['bids'] = [[float(x) for x in y] for y in  resp['bids']]
-                    order_book['asks'] = [[float(x) for x in y] for y in  resp['asks']]
-                    await add_diff(q, order_book)
+                    orderbook['lastUpdateId'] = resp['lastUpdateId']
+                    orderbook['bids'] = [[float(x) for x in y] for y in  resp['bids']]
+                    orderbook['asks'] = [[float(x) for x in y] for y in  resp['asks']]
+                    await add_diff(q, orderbook)
         except Exception as e:
             print('Order book snapshot error:', e)
 
@@ -48,89 +47,71 @@ async def queue_stream(q: asyncio.Queue) -> None:
         except Exception as e:
             print('Websocket error:', e)
 
-async def add_diff(q: asyncio.Queue, order_book: dict) -> None:
-    #Keep looping until getting an event that intersects local orderbook
-    while True:
-        item = await q.get()
-        #if last update ID of event is smaller than local orderbook ID, drop event
-        if item['u'] <= order_book['lastUpdateId']: q.task_done()
-        #Only process event if its update IDs overlap the update ID of local orderbook
-        if item['U'] <= order_book['lastUpdateId'] + 1 and item['u'] >= order_book['lastUpdateId'] + 1:
+async def add_diff(q: asyncio.Queue, orderbook: dict) -> None:
+    def update_orderbook() -> None:
             for level in item['b']:
                 bid_set = False
 
-                for i in order_book['bids']:
+                for i in orderbook['bids']:
                     if float(level[0]) == i[0]:
                         i[1] = float(level[1])
                         bid_set = True
                         break
 
-                if not bid_set: order_book['bids'].append([float(level[0]), float(level[1])])
+                if not bid_set: orderbook['bids'].append([float(level[0]), float(level[1])])
 
             for level in item['a']:
                 ask_set = False
 
-                for i in order_book['asks']:
+                for i in orderbook['asks']:
                     if float(level[0]) == i[0]:
                         i[1] = float(level[1])
                         ask_set = True
                         break
 
-                if not ask_set:order_book['asks'].append([float(level[0]), float(level[1])])
+                if not ask_set:orderbook['asks'].append([float(level[0]), float(level[1])])
 
-            order_book['lastUpdateId'] = item['u']
-            order_book['bids'] = sorted([x for x in order_book['bids'] if x[1] != 0], key = lambda x: x[0], reverse=True)
-            order_book['asks'] = sorted([x for x in order_book['asks'] if x[1] != 0], key = lambda x: x[0])
+            orderbook['lastUpdateId'] = item['u']
+            orderbook['bids'] = sorted([x for x in orderbook['bids'] if x[1] != 0], key = lambda x: x[0], reverse=True)
+            orderbook['asks'] = sorted([x for x in orderbook['asks'] if x[1] != 0], key = lambda x: x[0])
             q.task_done()
-            print(order_book)
+            print(orderbook)
+        
+    #Keep looping until getting an event that intersects local orderbook
+    while True:
+        item = await q.get()
+        
+        #if last update ID of event is smaller than local orderbook ID, drop event
+        if item['u'] <= orderbook['lastUpdateId']:
+            q.task_done()
+            continue
+        
+        #Only process event if its update IDs overlap the update ID of local orderbook
+        if item['U'] <= orderbook['lastUpdateId'] + 1 and item['u'] >= orderbook['lastUpdateId'] + 1:
+            update_orderbook()
             break
 
     while True:
-
         item = await q.get()
-        if item['u'] <= order_book['lastUpdateId']: q.task_done()
+        if item['u'] <= orderbook['lastUpdateId']:
+            q.task_done()
+            continue
 
-        for level in item['b']:
-            bid_set = False
-
-            for i in order_book['bids']:
-                if float(level[0]) == i[0]:
-                    i[1] = float(level[1])
-                    bid_set = True
-                    break
-
-            if not bid_set: order_book['bids'].append([float(level[0]), float(level[1])])
-
-        for level in item['a']:
-            ask_set = False
-
-            for i in order_book['asks']:
-                if float(level[0]) == i[0]:
-                    i[1] = float(level[1])
-                    ask_set = True
-                    break
-
-            if not ask_set:order_book['asks'].append([float(level[0]), float(level[1])])
-
-        order_book['lastUpdateId'] = item['u']
-        order_book['bids'] = sorted([x for x in order_book['bids'] if x[1] != 0], key = lambda x: x[0], reverse=True)
-        order_book['asks'] = sorted([x for x in order_book['asks'] if x[1] != 0], key = lambda x: x[0])
-        q.task_done()
-        print(order_book)
+        update_orderbook()
 
 async def main() -> None:
     print('Program started.')
     q = asyncio.Queue()
-    order_book = {'bids':[], 'asks': [], 'lastUpdateId': 0}
+    orderbook = {'bids':[], 'asks': [], 'lastUpdateId': 0}
     try:
-        await asyncio.gather(queue_stream(q), depth_snapshot(q, order_book))
+        await asyncio.gather(queue_stream(q), depth_snapshot(q, orderbook))
     except asyncio.CancelledError as e:
         print('Tasks cancelled.', e)
 
     while True:
         try:
             print('Program restarted.')
-            await asyncio.gather(queue_stream(q), depth_snapshot(q, order_book))
+            await asyncio.gather(queue_stream(q), depth_snapshot(q, orderbook))
         except asyncio.CancelledError as e:
             print('Tasks cancelled.', e)
 
